69db8594fcd500f83793b7b986e90bb6741db880
Alfred Matrimonio <30339882+pinoynukerz@users.noreply.github.com>
2022-10-25 15:25:41 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,96 +1,66 @@
-<?php
-/**
- * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * This file is licensed under the Apache License, Version 2.0 (the "License").
- * You may not use this file except in compliance with the License. A copy of
- * the License is located at
- *
- * http://aws.amazon.com/apache2.0/
- *
- * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
- * CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
- */
-require 'vendor/autoload.php';
+<!DOCTYPE html>
+<html lang="en">
 
-use PHPMailer\PHPMailer\PHPMailer;
-use Aws\Ses\SesClient;
-use Aws\Ses\Exception\SesException;
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-// Replace sender@example.com with your "From" address.
-// This address must be verified with Amazon SES.
-$sender = 'jmattz23@hotmail.com';
-$sendername = 'Sender Name';
+  <title>Knight Bootstrap Template - Index</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
 
-// Replace recipient@example.com with a "To" address. If your account
-// is still in the sandbox, this address must be verified.
-$recipient = 'recipient@example.com';
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
-// Specify a configuration set.
-$configset = 'ConfigSet';
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
-$subject = 'List of customers to contact';
+  <!-- Vendor CSS Files -->
+  <link href="http://ec2-100-25-182-244.compute-1.amazonaws.com/html-main/assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="http://ec2-100-25-182-244.compute-1.amazonaws.com/html-main/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="http://ec2-100-25-182-244.compute-1.amazonaws.com/html-main/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="http://ec2-100-25-182-244.compute-1.amazonaws.com/html-main/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="http://ec2-100-25-182-244.compute-1.amazonaws.com/html-main/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="http://ec2-100-25-182-244.compute-1.amazonaws.com/html-main/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
-$htmlbody = <<<EOD
-<html>
-<head></head>
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Knight - v4.9.1
+  * Template URL: https://bootstrapmade.com/knight-free-bootstrap-theme/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
 <body>
-<h1>Hello!</h1>
-<p>Please see the attached file for a list of customers to contact.</p>
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero">
+    <div class="hero-container">
+      <a href="index.html" class="hero-logo" data-aos="zoom-in"><img src="assets/img/hero-logo.png" alt=""></a>
+      <h1 data-aos="zoom-in">AUDIOPHILE TEST FORM</h1>
+      <h2 data-aos="fade-up">We are team of talented designers making websites with Bootstrap</h2>
+        
+      <a data-aos="fade-up" data-aos-delay="200" href="" class="btn-get-started scrollto">Get Started</a>
+    </div>
+  </section><!-- End Hero -->
+    
+      <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="http://ec2-100-25-182-244.compute-1.amazonaws.com/html-main/assets/vendor/aos/aos.js"></script>
+  <script src="http://ec2-100-25-182-244.compute-1.amazonaws.com/html-main/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="http://ec2-100-25-182-244.compute-1.amazonaws.com/html-main/assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="http://ec2-100-25-182-244.compute-1.amazonaws.com/html-main/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="http://ec2-100-25-182-244.compute-1.amazonaws.com/html-main/assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="http://ec2-100-25-182-244.compute-1.amazonaws.com/html-main/assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
 </body>
+
 </html>
-EOD;
-
-$textbody = <<<EOD
-Hello,
-Please see the attached file for a list of customers to contact.
-EOD;
-
-// The full path to the file that will be attached to the email.
-$att = 'path/to/customers-to-contact.xlsx';
-
-// Create an SesClient.
-$client = new SesClient([
-    'profile' => 'default',
-    'region' => 'us-west-2',
-    'version' => '2010-12-01'
-]);
-
-// Create a new PHPMailer object.
-$mail = new PHPMailer;
-
-// Add components to the email.
-$mail->setFrom($sender, $sendername);
-$mail->addAddress($recipient);
-$mail->Subject = $subject;
-$mail->Body = $htmlbody;
-$mail->AltBody = $textbody;
-$mail->addAttachment($att);
-$mail->addCustomHeader('X-SES-CONFIGURATION-SET', $configset);
-
-// Attempt to assemble the above components into a MIME message.
-if (!$mail->preSend()) {
-    echo $mail->ErrorInfo;
-} else {
-    // Create a new variable that contains the MIME message.
-    $message = $mail->getSentMIMEMessage();
-}
-
-// Try to send the message.
-try {
-    $result = $client->sendRawEmail([
-        'RawMessage' => [
-            'Data' => $message
-        ]
-    ]);
-    // If the message was sent, show the message ID.
-    $messageId = $result->get('MessageId');
-    echo("Email sent! Message ID: $messageId" . "\n");
-} catch (SesException $error) {
-    // If the message was not sent, show a message explaining what went wrong.
-    echo("The email was not sent. Error message: "
-        . $error->getAwsErrorMessage() . "\n");
-} 
-
-?>
