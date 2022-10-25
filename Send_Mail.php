68ae672b64af321014ca5035c3d0d6da2483b6ad
Alfred Matrimonio <30339882+pinoynukerz@users.noreply.github.com>
2022-10-25 13:58:49 +0800

    Update Send_Mail.php

--- Send_Mail.php
+++ Send_Mail.php
@@ -7,10 +7,10 @@ $mail = new PHPMailer();
 $mail->IsSMTP(true); // SMTP
 $mail->SMTPAuth   = true;  // SMTP authentication
 $mail->Mailer = "smtp";
-$mail->Host= "tls://email-smtp.us-east-1.amazonaws.com"; // Amazon SES
+$mail->Host= "tls://smtp.gmail.com"; // Amazon SES
 $mail->Port = 465;  // SMTP Port
-$mail->Username = "AKIAVFEKWWJKOCX3NVOC";  // SMTP  Username
-$mail->Password = "BNHBX65+DorQDmmi9T95DCei5oH6KM6M1umgyt4Pht4i";  // SMTP Password
+$mail->Username = "";  // SMTP  Username
+$mail->Password = "";  // SMTP Password
 $mail->SetFrom($from, 'From Name');
 $mail->AddReplyTo($from,'9lessons Labs');
 $mail->Subject = $subject;
